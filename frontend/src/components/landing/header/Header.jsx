6c507a4059820159f72092e4288e8f1718084202
Thibaud <thibaud.brault@protonmail.com>
2022-12-01 20:28:39 +0100

    Finished desktop landing page

--- frontend/src/components/landing/header/Header.jsx
+++ frontend/src/components/landing/header/Header.jsx
@@ -1,35 +1,55 @@
-import React from 'react';
-import headerImg from '@assets/landing page/image1.svg'
+import React from "react";
+import PropTypes from "prop-types";
+import headerImg from "@assets/landing page/image1.svg";
 
 function Header({ userType }) {
-    return (
-        userType === 'parent' ? (
-            <header className='header'>
-                <h1>Réservez une place auprès de professionnelles de la petite enfance gratuitement en quelques clics</h1>
-                <div className='headerInputs'>
-                    <input type="text" name="type" id="type" placeholder='Assistante maternelle, crèche, ...' />
-                    <input type="text" name="date" id="date" placeholder='Date' />
-                    <input type="text" name="place" id="place" placeholder='Où' />
-                    <button type="submit">Rechercher <span>➜</span></button>
-                </div>
-            </header>
-        ) : (
-            <header className='headerPro'>
-                <div className='headerProMain'>
-                    <h1>Babyplace</h1>
-                    <p>
-                        Gagnez du temps en optimisant votre agenda au quotdien<br />
-                        Installez le logiciel Babyplace maintenant pour saisir toutes les opportunités autour de chez vous<br />
-                        Souscription au logiciel Babyplace sans engagement<br />
-                        Logiciel facile a utiliser pour vous permettre d’optimiser vos rendez-vous<br />
-                        Communication directe et simplifiée avec les parents<br />
-                    </p>
-                    <button>En savoir plus</button>
-                </div>
-                <img src={headerImg} alt="" />
-            </header>
-        )
-    )
+  return userType === "parent" ? (
+    <header className="header">
+      <h1>
+        Réservez une place auprès de professionnelles de la petite enfance
+        gratuitement en quelques clics
+      </h1>
+      <div className="headerInputs">
+        <input
+          type="text"
+          name="type"
+          id="type"
+          placeholder="Assistante maternelle, crèche, ..."
+        />
+        <input type="text" name="date" id="date" placeholder="Date" />
+        <input type="text" name="place" id="place" placeholder="Où" />
+        <button type="submit">
+          Rechercher <span>➜</span>
+        </button>
+      </div>
+    </header>
+  ) : (
+    <header className="headerPro">
+      <div className="headerProMain">
+        <h1>Babyplace</h1>
+        <p>
+          Gagnez du temps en optimisant votre agenda au quotdien
+          <br />
+          Installez le logiciel Babyplace maintenant pour saisir toutes les
+          opportunités autour de chez vous
+          <br />
+          Souscription au logiciel Babyplace sans engagement
+          <br />
+          Logiciel facile a utiliser pour vous permettre d’optimiser vos
+          rendez-vous
+          <br />
+          Communication directe et simplifiée avec les parents
+          <br />
+        </p>
+        <button type="button">En savoir plus</button>
+      </div>
+      <img src={headerImg} alt="" />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
+
+Header.propTypes = {
+  userType: PropTypes.string.isRequired,
+};
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,8 +10,8 @@ import copie from "@assets/img-copie.svg";
 import imgInfo from "@assets/landing page/image2.svg";
 
 export default function Home() {
-
-  const [userType, setUserType] = useState("parent")
+  const [userType, setUserType] = useState("parent");
+  const [carouselFirst, setCarouselFirst] = useState(true);
 
   return (
     <>
@@ -19,19 +19,31 @@ export default function Home() {
       <Header userType={userType} />
       {userType === "parent" ? (
         <main className="home">
-          <h2>Simplifiez vous la vie avec Babyplace grace a notre logiciel de reservation en ligne</h2>
+          <h2>
+            Simplifiez vous la vie avec Babyplace grace a notre logiciel de
+            reservation en ligne
+          </h2>
           <ul className="mainGrid">
             <li>
               <img src={dossier} alt="" />
-              <p>Accédez aux disponibilités de dizaine de miliers de professionnelles de la petite enfance</p>
+              <p>
+                Accédez aux disponibilités de dizaine de miliers de
+                professionnelles de la petite enfance
+              </p>
             </li>
             <li>
               <img src={time} alt="" />
-              <p>Réservez en ligne, prenez rdv  24h/24 et 7j/7 avec des crèches ou assistantes maternelles</p>
+              <p>
+                Réservez en ligne, prenez rdv 24h/24 et 7j/7 avec des crèches ou
+                assistantes maternelles
+              </p>
             </li>
             <li>
               <img src={key} alt="" />
-              <p>Retrouvez votre historique de réservation et votre dossier petite enfance</p>
+              <p>
+                Retrouvez votre historique de réservation et votre dossier
+                petite enfance
+              </p>
             </li>
           </ul>
           <section className="mainApp">
@@ -42,12 +54,27 @@ export default function Home() {
             <img src={copie} alt="" />
           </section>
           <ul className="mainDesc">
-            <li>Selectionnez facilement les modes de garde grâce a notre agenda intelligent</li>
-            <li>Choisissez vos modes de garde en fonction des commentaires et notes d’autres parents</li>
-            <li>Utilisez le contrat modèle de BabyPlace pour assistante maternelle  afin de vous assurer une protection maximale</li>
-            <li>Soyez averti par notifications en temps réel des changements possibles de l’agenda des crèches ou ASM</li>
+            <li>
+              Selectionnez facilement les modes de garde grâce a notre agenda
+              intelligent
+            </li>
+            <li>
+              Choisissez vos modes de garde en fonction des commentaires et
+              notes d’autres parents
+            </li>
+            <li>
+              Utilisez le contrat modèle de BabyPlace pour assistante maternelle
+              afin de vous assurer une protection maximale
+            </li>
+            <li>
+              Soyez averti par notifications en temps réel des changements
+              possibles de l’agenda des crèches ou ASM
+            </li>
             <li>Annulez ou modifiez vos rendez-vous en 2 clics</li>
-            <li>Notez et partagez vos expériences directement sur BabyPlace ce qui vous donne une plus grande transparence</li>
+            <li>
+              Notez et partagez vos expériences directement sur BabyPlace ce qui
+              vous donne une plus grande transparence
+            </li>
           </ul>
         </main>
       ) : (
@@ -56,23 +83,26 @@ export default function Home() {
             <img src={imgInfo} alt="" />
             <div>
               <h2>
-                Simplifiez vous la vie en choisissant un systeme de reservation moderne et efficace
+                Simplifiez vous la vie en choisissant un systeme de reservation
+                moderne et efficace
               </h2>
               <p>
-                Gerez vos reservations directement depuis votre agenda que vous pouvez consulter sur votre telephone, ordinateur ou tablette<br />
-                Soyez prevenu en temps reel des annulations vous permettant ainsi  de remplacer les places vacantes
-                Remplacez facilement les absences grace aux “waiting list” des parents
+                Gerez vos reservations directement depuis votre agenda que vous
+                pouvez consulter sur votre telephone, ordinateur ou tablette
+                <br />
+                Soyez prevenu en temps reel des annulations vous permettant
+                ainsi de remplacer les places vacantes Remplacez facilement les
+                absences grace aux “waiting list” des parents
               </p>
             </div>
           </section>
           <section className="homeProGrid">
             <div className="homeProGridTitle">
               <h2>
-                Equipez vous du logiciel de gestion de place d’accueil de jeunes enfants le plus complet
+                Equipez vous du logiciel de gestion de place d’accueil de jeunes
+                enfants le plus complet
               </h2>
-              <p>
-                Cliquez sur l’une de nos solutions pour en savoir plus
-              </p>
+              <p>Cliquez sur l’une de nos solutions pour en savoir plus</p>
             </div>
             <ul>
               <li>
@@ -80,9 +110,14 @@ export default function Home() {
                 <div>
                   <h3>Agenda en ligne</h3>
                   <p>
-                    Consultez en temps reel votre agenda et modifiez le en quelques cliques afin d’optimiser votre temps et votre rentabilite. Accessible de puis votre Smartphone, tabelle ou Telephone
+                    Consultez en temps reel votre agenda et modifiez le en
+                    quelques cliques afin d’optimiser votre temps et votre
+                    rentabilite. Accessible de puis votre Smartphone, tabelle ou
+                    Telephone
                   </p>
-                  <button>En savoir plus <span>➜</span></button>
+                  <button type="button">
+                    En savoir plus <span>➜</span>
+                  </button>
                 </div>
               </li>
               <li>
@@ -90,9 +125,12 @@ export default function Home() {
                 <div>
                   <h3>Soyez alertée</h3>
                   <p>
-                    Choisissez le mode de notifications afin d’etre informe au plus vote des annulations et des demandes de reservation.
+                    Choisissez le mode de notifications afin d’etre informe au
+                    plus vote des annulations et des demandes de reservation.
                   </p>
-                  <button>En savoir plus <span>➜</span></button>
+                  <button type="button">
+                    En savoir plus <span>➜</span>
+                  </button>
                 </div>
               </li>
               <li>
@@ -100,9 +138,13 @@ export default function Home() {
                 <div>
                   <h3>Marketing de votre activité</h3>
                   <p>
-                    Optimisez votre page profil pour vous rendre plus visible et vous permettre de vous decouvrir par les parents de votre quartier.
+                    Optimisez votre page profil pour vous rendre plus visible et
+                    vous permettre de vous decouvrir par les parents de votre
+                    quartier.
                   </p>
-                  <button>En savoir plus <span>➜</span></button>
+                  <button type="button">
+                    En savoir plus <span>➜</span>
+                  </button>
                 </div>
               </li>
               <li>
@@ -110,10 +152,14 @@ export default function Home() {
                 <div>
                   <h3>Communiquez avec les parents</h3>
                   <p>
-                    Les parents doivent avoir prerempliss leur liste de documents pour pouvoir faire des reservations
-                    Les parents peuvent vous soumettre les contrats deja valide par les equipes legales de Babyplabce
+                    Les parents doivent avoir prerempliss leur liste de
+                    documents pour pouvoir faire des reservations Les parents
+                    peuvent vous soumettre les contrats deja valide par les
+                    equipes legales de Babyplabce
                   </p>
-                  <button>En savoir plus <span>➜</span></button>
+                  <button type="button">
+                    En savoir plus <span>➜</span>
+                  </button>
                 </div>
               </li>
             </ul>
@@ -121,23 +167,60 @@ export default function Home() {
           <section className="homeProCarousel">
             <h2>Témoignages</h2>
             <ul className="homeProTestimonies">
-              <li>
-                <p>User</p>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis assumenda aliquam molestiae rem quia velit impedit quod cum aperiam eveniet quis exercitationem architecto tenetur illo, perferendis sunt ab eius dolor.
-                </p>
-              </li>
-              <li>
-                <p>User</p>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis assumenda aliquam molestiae rem quia velit impedit quod cum aperiam eveniet quis exercitationem architecto tenetur illo, perferendis sunt ab eius dolor.
-                </p>
-              </li>
+              {carouselFirst ? (
+                <>
+                  <li>
+                    <p>User 1</p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nobis assumenda aliquam molestiae rem quia velit impedit
+                      quod cum aperiam eveniet quis exercitationem architecto
+                      tenetur illo, perferendis sunt ab eius dolor.
+                    </p>
+                  </li>
+                  <li>
+                    <p>User 2</p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nobis assumenda aliquam molestiae rem quia velit impedit
+                      quod cum aperiam eveniet quis exercitationem architecto
+                      tenetur illo, perferendis sunt ab eius dolor.
+                    </p>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li>
+                    <p>User 3</p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nobis assumenda aliquam molestiae rem quia velit impedit
+                      quod cum aperiam eveniet quis exercitationem architecto
+                      tenetur illo, perferendis sunt ab eius dolor.
+                    </p>
+                  </li>
+                  <li>
+                    <p>User 4</p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nobis assumenda aliquam molestiae rem quia velit impedit
+                      quod cum aperiam eveniet quis exercitationem architecto
+                      tenetur illo, perferendis sunt ab eius dolor.
+                    </p>
+                  </li>
+                </>
+              )}
             </ul>
+            <button
+              type="button"
+              onClick={() => setCarouselFirst(!carouselFirst)}
+            >
+              ➜
+            </button>
           </section>
         </main>
       )}
-      <Footer />
+      <Footer userType={userType} />
     </>
   );
 }
