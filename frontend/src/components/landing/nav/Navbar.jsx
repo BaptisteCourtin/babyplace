82849289c9a8621b72244dc16b60ae05718edc19
GuiPich <110974962+GuiPich@users.noreply.github.com>
2022-12-02 12:46:52 +0100

    ajout du logo sur la navbar

--- frontend/src/components/landing/nav/Navbar.jsx
+++ frontend/src/components/landing/nav/Navbar.jsx
@@ -1,57 +1,58 @@
 import React from "react";
 import PropTypes from "prop-types";
-import logo from "@assets/logo.svg";
+import logo from "@assets/logo4.svg";
+import logotxt from "@assets/babyplacetxt.svg";
 
 function Navbar({ userType, setUserType }) {
-  return userType === "parent" ? (
-    <nav className="nav">
-      <img src={logo} alt="Babyplace" />
-      <button
-        type="button"
-        className="textBtn"
-        onClick={() => setUserType("pro")}
-      >
-        Vous êtes professionnel de la petite enfance ?
-      </button>
-      <div className="navRight">
-        <button type="button" className="textBtn">
-          Besoin d'aide ?
-        </button>
-        <button type="button" className="connect">
-          <div>
-            <span>Se connecter</span>
-            <br />
-            <span>Gérer mes rdv</span>
-          </div>
-          <span className="arrow">➜</span>
-        </button>
-      </div>
-    </nav>
-  ) : (
-    <nav className="nav pro">
-      <img src={logo} alt="Babyplace" />
-      <ul>
-        <button
-          type="button"
-          className="textBtn"
-          onClick={() => setUserType("parent")}
-        >
-          Vous êtes un parent ?
-        </button>
-      </ul>
-      <div className="navBtn">
-        <button type="button">S'enregistrer</button>
-        <button type="button">
-          Se connecter <span>➜</span>
-        </button>
-      </div>
-    </nav>
-  );
+    return userType === "parent" ? (
+        <nav className="nav">
+            <div class="logoContainer"><img src={logo} alt="Babyplace" id="logoCoeur" /><img src={logotxt} alt="Babyplace" id="logotxt" /></div>
+            <button
+                type="button"
+                className="textBtn"
+                onClick={() => setUserType("pro")}
+            >
+                Vous êtes professionnel de la petite enfance ?
+            </button>
+            <div className="navRight">
+                <button type="button" className="textBtn">
+                    Besoin d'aide ?
+                </button>
+                <button type="button" className="connect">
+                    <div>
+                        <span>Se connecter</span>
+                        <br />
+                        <span>Gérer mes rdv</span>
+                    </div>
+                    <span className="arrow">➜</span>
+                </button>
+            </div>
+        </nav>
+    ) : (
+        <nav className="nav pro">
+            <img src={logo} alt="Babyplace" />
+            <ul>
+                <button
+                    type="button"
+                    className="textBtn"
+                    onClick={() => setUserType("parent")}
+                >
+                    Vous êtes un parent ?
+                </button>
+            </ul>
+            <div className="navBtn">
+                <button type="button">S'enregistrer</button>
+                <button type="button">
+                    Se connecter <span>➜</span>
+                </button>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
 
 Navbar.propTypes = {
-  userType: PropTypes.string.isRequired,
-  setUserType: PropTypes.func.isRequired,
+    userType: PropTypes.string.isRequired,
+    setUserType: PropTypes.func.isRequired,
 };
