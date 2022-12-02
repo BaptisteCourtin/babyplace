98c4d1999de8ba9e74ea48f3c421eb33d7fc1ac6
AMELIE GUILLOUX <amelie.guilloux@hotmail.fr>
2022-12-02 14:26:35 +0100

    scss form created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Appli from "@pages/AppliTuto";
 import AppliHome from "@pages/AppliHome";
 import AppliSearch from "@pages/AppliSearch";
+import FormStructure from "@pages/FormStructure";
 
 import Home from "./pages/Home";
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/appli" element={<Appli />} />
         <Route path="/appli/home" element={<AppliHome />} />
         <Route path="/appli/search" element={<AppliSearch />} />
+        <Route path="/structure/inscription-form" element={<FormStructure />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/pages/FormStructure.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../components/form/formStructure.scss'
+const FormStructure = () => {
+    return (
+        <div className="formStructureContainer">
+            <form className="formStrucure">
+
+            </form>
+        </div>
+    );
+};
+
+export default FormStructure;
