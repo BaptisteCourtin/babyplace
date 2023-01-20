600a196f7ada5b2fc4f9770d3c7f760e135d2235
GuiPich <110974962+GuiPich@users.noreply.github.com>
2023-01-20 09:53:59 +0100

    bug fix

--- backend/src/services/messagerie.service.js
+++ backend/src/services/messagerie.service.js
@@ -1,17 +1,6 @@
 
 const { Server } = require("socket.io");
 
-let sockets = [];
-
-const findSocket = (id) => {
-    return sockets.find((element) => element.id === id);
-};
-
-const removeSocket = (socketId) => {
-    let i = sockets.findIndex((element) => element.socket.id === socketId);
-    if (i != -1) sockets.splice(i, 1);
-};
-
 const open = () => {
 
     let ws = new Server({
@@ -24,25 +13,13 @@ const open = () => {
     ws.on("connection", (socket) => {
         console.log(`User ${socket.id} is connected on the website`);
 
-        socket.on("auth", (data) => {
-            let s = findSocket(data);
-            if (s) removeSocket(s.socket.id);
-
-            sockets.push({
-                id: data,
-                date: Date.now(),
-                socket: socket
-            });
-            console.log(sockets[0])
-            console.log(`WebSocket clientId connected is : ${sockets[0].id}`);
-        });
 
 
         socket.on("join_room", (room) => {
             socket.join(room);
-            let id = sockets[0].id;
+            let id = socket.id;
             socket.emit("is_logged", (id));
-            console.log(`User with id: ${sockets[0].id} joined the room: ${room}`);
+            console.log(`User with id: ${socket.id} joined the room: ${room}`);
         });
 
         socket.on("send_message", (data) => {
@@ -57,8 +34,8 @@ const open = () => {
     ws.listen(3001, () => {
         console.log("Chat server is running on 3001");
     });
-
 }
+
 
 module.exports = {
     open
