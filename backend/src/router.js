cfc58724591cbae853b9a79bcff5acf0f5317791
Thibaud-Brault <thibaud.brault@protonmail.com>
2023-01-20 10:12:38 +0100

    bugfix

--- backend/src/router.js
+++ backend/src/router.js
@@ -1268,8 +1268,10 @@ router.post("/auth", async (req, res) => {
             console.error(err);
             res.status(500).send("Erreur de connexion");
           });
-      } else {
+      } else if (user && req.body.password !== user.password) {
         res.status(401).send("Email ou mot de passe incorrect");
+      } else {
+        res.status(404).send("Votre compte n'est pas encore vérifié")
       }
     })
     .catch((err) => {
